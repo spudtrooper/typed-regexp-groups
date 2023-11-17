87953796cb9c5bce7b29283c222948b21317ad28
Jeff Palm <jeff@jeffpalm.com>
2023-11-16 22:25:55 -0500

    update Thu Nov 16 22:25:55 EST 2023

--- integtest/local/jest.config.js
+++ integtest/local/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testMatch: ["**/*.integ.ts"]
+  testMatch: ["**/*.integ.ts"],
+  watchman: false,
 };
---
+++ integtest/remote/scripts/test-fresh.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+scripts=$(dirname $0)
+root=$(realpath $scripts/..)
+
+get_version() {
+  local package_json_file=$root/node_modules/typed-regexp-groups/package.json
+  local version=$(cat $package_json_file | jq -r '.version')
+  echo $version
+}
+
+before_version=$(get_version)
+rm -rf node_modules
+yarn install
+after_version=$(get_version)
+yarn jest
+
+echo "before_version: $before_version"
+echo "after_version: $after_version"
