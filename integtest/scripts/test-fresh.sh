f580a8ac624e6fc49110e7ae3f52e04adae51028
Jeff Palm <jeff@jeffpalm.com>
2023-11-17 08:15:34 -0500

    update Fri Nov 17 08:15:34 EST 2023

--- integtest/scripts/test-fresh.sh
+++ integtest/scripts/test-fresh.sh
@@ -6,6 +6,9 @@ root=$(pwd)
 
 get_version() {
   local package_json_file=$root/node_modules/typed-regexp-groups/package.json
+  if [ ! -f $package_json_file ]; then
+    package_json_file=../../package.json
+  fi
   local version=$(cat $package_json_file | jq -r '.version')
   echo $version
 }
